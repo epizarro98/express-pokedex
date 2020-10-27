d90a4c81d1f1078d50f5d3c997a4849d53a207c6
epizarro98 <epizarro309@gmail.com>
2020-10-27 18:58:12 -0400

    hw

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const axios = require('axios');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 const port = process.env.PORT || 3000;
+const db = require("./models");
 
 app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(ejsLayouts);
+app.use(express.static(__dirname + '/public'));
 
 // GET / - main index of site
 app.get('/', function(req, res) {
@@ -17,6 +19,7 @@ app.get('/', function(req, res) {
   axios.get(pokemonUrl).then( function(apiResponse) {
     const pokemon = apiResponse.data.results;
     res.render('index', { pokemon: pokemon.slice(0, 151) });
+    console.log(pokemonUrl)
   })
 });
 
@@ -26,5 +29,6 @@ app.use('/pokemon', require('./routes/pokemon'));
 const server = app.listen(port, function() {
   console.log('...listening on', port );
 });
+
 
 module.exports = server;
